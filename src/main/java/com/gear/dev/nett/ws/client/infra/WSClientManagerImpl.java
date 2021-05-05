cc2a22973283c4da419de8222020154d67f3177f
Guerino Rodella <guerino.rodella@ftdata.com.br>
2021-05-05 17:37:22 -0300

    Refatoração mudando o nome da Implementação - NettyWSClientManager.

--- src/main/java/com/gear/dev/nett/ws/client/infra/WSClientManagerImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gear.dev.nett.ws.client.infra;
-
-import com.gear.dev.nett.ws.client.core.WSClientManager;
-
-/**
- * @author guerinorodella
- */
-public class WSClientManagerImpl implements WSClientManager {
-}
---
+++ src/main/java/com/gear/dev/nett/ws/client/infra/netty/NettyWSClientManager.java
@@ -0,0 +1,17 @@
+package com.gear.dev.nett.ws.client.infra.netty;
+
+import com.gear.dev.nett.ws.client.core.ClientData;
+import com.gear.dev.nett.ws.client.core.WSClientManager;
+
+import java.util.List;
+
+/**
+ * @author guerinorodella
+ */
+public class WSClientManagerImpl implements WSClientManager {
+
+    @Override
+    public void start(List<ClientData> clientDataList) {
+
+    }
+}
