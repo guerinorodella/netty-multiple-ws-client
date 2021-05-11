704aab60be210c3fa3250345c4835e39457eaef7
Guerino Rodella <guerino.rodella@ftdata.com.br>
2021-05-10 22:38:11 -0300

    Criado classe Util para logar horário.

---
+++ src/main/java/com/gear/dev/nett/ws/client/infra/Util.java
@@ -0,0 +1,23 @@
+package com.gear.dev.nett.ws.client.infra;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author guerinorodella
+ */
+public class Util {
+    private static SimpleDateFormat sdf;
+
+    public static String currentDateTime() {
+        return getSDF().format(new Date());
+    }
+
+    private static SimpleDateFormat getSDF() {
+        if (sdf != null) {
+            return sdf;
+        }
+        sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss:sss");
+        return getSDF();
+    }
+}
